5e5f0093155564cb262a3da1535f010599972adc
Shiv Chauhan <shivchauhan795@gmail.com>
2023-05-28 11:13:32 +0530

    Logic added

--- Stack/Next_Smaller_Element.cpp
+++ Stack/Next_Smaller_Element.cpp
@@ -1,12 +1,16 @@
 // We have to find next smaller element for every element in the array.
 // For eg:  [2,1,4,3] is an array then the output array will be -> [1,-1,3,-1]
 
+/*
+    Logic is that we traverse the given arr in reverse order and check each element to top element of the stack. If top() is smaller then, push top() into ans array and push element of the arr in stack, else pop till top is smaller and repeat the same.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     vector<int> arr = {2, 1, 4, 3};
+    
     stack<int> st;
     st.push(-1);
     vector<int> ans(arr.size());
@@ -17,8 +21,11 @@ int main()
         ans[i] = st.top();
         st.push(arr[i]);
     }
+
+    // printing array
     for(int i = 0; i < ans.size(); i++){
         cout << ans[i] << " ";
     } 
+    
     return 0;
 }
